248fe573daa64237c2460427a2208b1a64ee3719
Olga <olga1998senko@gmail.com>
2023-03-26 21:10:52 +0300

    main

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const a = 21;
+const b = 3;
+const add = (a, b) => a + b;
+console.log(add(a, b));
+const subtract = (a, b) => a - b;
+console.log(subtract(a, b));
+const multiplication = (a, b) => a * b;
+console.log(multiplication(a, b));
